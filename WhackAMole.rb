670e24dda23b9ec6bf0c1635d8033444149ef5c0
tihomir <tihomirdzelebdzic@gmail.com>
2021-05-18 01:11:35 -0400

    Updated constructor

--- WhackAMole.rb
+++ WhackAMole.rb
@@ -3,8 +3,11 @@ require 'gosu'
 class Mole < Gosu::Window 
 
     def initialize 
-        super 640, 480
+        super 800, 600
         self.caption = "Tutorial Game"
+        @hammer = Gosu::Image.new('Sprites/hammer.png')
+        @ruby = Gosu::Image.new('Sprites/ruby.png')
+        @font = Gosu::Font.new(30)
 
     end
 
@@ -13,8 +16,14 @@ class Mole < Gosu::Window
     end
 
     def draw
+        @hammer.draw(mouse_x-80,mouse_y-38,2)
+        @ruby.draw(400,400,1)
+        @font.draw("WhackAnIdiot",0,0,0)
 
 
     end
 
 end
+
+lady = Mole.new
+lady.show
