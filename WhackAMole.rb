33e23a2cd48c8f1889de6eecee5edb0c50065776
tihomir <tihomirdzelebdzic@gmail.com>
2021-05-18 00:41:22 -0400

    Trening

--- WhackAMole.rb
+++ WhackAMole.rb
@@ -1,1 +1,20 @@
-puts "Hello world"
+require 'gosu'
+
+class Mole < Gosu::Window 
+
+    def initialize 
+        super 640, 480
+        self.caption = "Tutorial Game"
+
+    end
+
+    def update
+
+    end
+
+    def draw
+
+
+    end
+
+end
